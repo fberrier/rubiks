76d80d9d545305e68c6107e8cf6aa7cb2fb1615d
Francois Berrier <Francois.Berrier@cantab.net>
2022-02-12 20:15:36 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
+########################################################################################################################
+# Francois Berrier - Royal Holloway University London - MSc Project 2022                                               #
+########################################################################################################################
 from setuptools import setup
-
-setup(
-    name='pyexample',
-    version='0.1.0',    
-    description='A example Python package',
-    url='https://github.com/shuds13/pyexample',
-    author='Stephen Hudson',
-    author_email='shudson@anl.gov',
-    license='BSD 2-clause',
-    packages=['pyexample'],
-    install_requires=['mpi4py>=2.0',
-                      'numpy',                     
-                      ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+########################################################################################################################
+setup(name='rubiks',
+      version='1.0',    
+      description='Royal Holloway | MSc in Artificial Intelligence',
+      url='https://github.com/fberrier/rubiks',
+      author='Francois Berrier',
+      author_email='francois.berrier@cantab.net',
+      license='None',
+      packages=['rubiks'], #install_requires=['numpy','tbd'],
 )
+########################################################################################################################

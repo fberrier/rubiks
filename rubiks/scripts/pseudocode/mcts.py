a0bb6b47f458e48bfff8a79bc9470acf218fc3e2
Francois Berrier <francois.berrier@cantab.net>
2022-09-06 12:32:20 +0100

    update

--- rubiks/scripts/pseudocode/mcts.py
+++ rubiks/scripts/pseudocode/mcts.py
@@ -1,20 +1,25 @@
-##########################################################################################
+#################################################################
 def MCTS(initial_node, time_out, q_v_network):
     """ pseudo code for Monte Carlo Tree Search """
     initialize_time_out(time_out)
     tree = Tree(initial_node)
     while True:
         check_time_out()
-        leaf = add_new_leaf(tree, q_v_network)
+        leaf = construct_path_to_new_leaf(tree, q_v_network)
         if leaf.is_goal():
             return BFS(tree) # path to leaf can usually be improved by BFS
-##########################################################################################
-def add_new_leaf(tree, q_v_network):
+#################################################################
+def construct_path_to_new_leaf(tree, q_v_network):
     leaf = tree.initial_node
     while leaf.expanded:
-        pass
-    leaf.expand() # add children of leaf to tree
-    leaf.update_penalties()
+        actions_proba, value = q_v_network(leaf)
+        historical_actions_count = leaf.actions_taken
+        leaf = leaf.choose_child(actions_proba,
+                                 value,
+                                 historical_actions_count)
+    # add children of leaf to tree
+    leaf.expand()
+    # penalize path taken to favour exploration
+    leaf.increment_historical_actions_count()
     return leaf
-##########################################################################################
-
+#################################################################
